54e08c4ebaafc4e0c56c6d9578d7739ead9f4d89
Uriel Munguia <urimungu@uat.edu>
2020-02-20 19:00:43 -0700

    PlayerController
    Added the Player Movement and Shooting

---
+++ Assets/Script/GunController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    //References
+    public Transform Target;
+    public Sprite Gun, GunShoot;
+    public float ReloadSpeed = 0.4f;
+
+    //Variables
+    private const float HeightOffset = 0.4f;
+    private const float Radius = 0.5f;
+
+    private bool _canMove;
+    private bool _isRunning;
+    private float _timer;
+    private Vector2 _center;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake() {
+        _spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        FollowTarget(Target);
+
+        if (!_isRunning && Time.time > _timer && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _timer = Time.time + ReloadSpeed;
+            _isRunning = true;
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        _spriteRenderer.sprite = GunShoot;
+        yield return new WaitForSeconds(0.1f);
+        _spriteRenderer.sprite = Gun;
+        _isRunning = false;
+    }
+
+    private void FollowTarget(Transform target) {
+        //Follows Player
+        _center = (Vector2)target.position + (Vector2.up * HeightOffset);
+        var camDistance = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - _center).magnitude;
+        var tempRadius = camDistance > Radius ? Radius : camDistance;
+        transform.position = _center + ((Vector2)transform.right * tempRadius);
+
+        //Flips the Sprite
+        _spriteRenderer.flipY = Input.mousePosition.x < Camera.main.WorldToScreenPoint(_center).x;
+
+        //Rotates to look at mouse
+        Vector2 newPos = Input.mousePosition - Camera.main.WorldToScreenPoint(_center);
+        float angle= Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Stats
+    [Header("Stats")]
+    public float Speed = 5;
+    public float JumpForce = 5;
+    public float RayCastLength = 0.3f;
+
+    public LayerMask Layers;
+
+    //Variables
+    private bool _canMove = true;
+    private bool _grounded;
+    private Rigidbody2D _rb2d;
+    private Animator _anim;
+
+    private void Awake(){
+        _rb2d = GetComponent<Rigidbody2D>();
+        _anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (_canMove) {
+            var horizontal = Input.GetAxisRaw("Horizontal");
+            var vertical = Input.GetAxisRaw("Vertical");
+            Movement(horizontal, vertical);
+        }
+
+        _grounded = CheckIfGrounded();
+        PlayAnimations();
+    }
+
+    private void Movement(float hor, float ver) {
+        //Moves left and right
+        transform.GetChild(0).GetComponent<SpriteRenderer>().flipX = Mathf.Abs(hor) > 0.1f ? hor < 0.1f : transform.GetChild(0).GetComponent<SpriteRenderer>().flipX;
+        _rb2d.velocity = new Vector2(Speed * hor, _rb2d.velocity.y);
+
+        //Stops if the player stops pressing stuff
+        if(Mathf.Abs(hor) < 0.1f)
+            _rb2d.velocity = new Vector2(0, _rb2d.velocity.y);
+
+        //Handles Jumping
+        if(Input.GetButtonDown("Jump") && _grounded)
+            _rb2d.velocity = new Vector2(_rb2d.velocity.x, JumpForce);
+    }
+
+    //Plays the Animations
+    private void PlayAnimations() {
+        _anim.SetFloat("Speed", Mathf.Abs(_rb2d.velocity.x));
+        _anim.SetBool("Grounded", _grounded);
+    }
+
+    private bool CheckIfGrounded() {
+        return Physics2D.Raycast(transform.position, Vector2.down, RayCastLength, Layers);
+    }
+}
